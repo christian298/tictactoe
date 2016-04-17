dd58e4459632d4e83a1104c6b7efcdbb28285300
Christian Sandvoss <christian.sandvoss@gmail.com>
2016-04-17 21:57:22 +0200

    added reducer

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,21 +1,23 @@
 import {SET_FIELD} from '../actions/actions.js';
-import TicTacToe from '../model/tictactoe.js';
+import {createBoard, getCordsForIndex} from '../helper/helper';
 
-let ttt = new TicTacToe(3);
+const BOARD_SIZE = 3;
 
 const initialState = {
-    board: ttt.getBoard()
+    board: createBoard(BOARD_SIZE),
+    player: 1
 };
 
 function tictactoe(state = initialState, action) {
     switch (action.type) {
         case SET_FIELD:
             let newBoard = state.board;
-            const cords = ttt.getCords(action.field);
-            newBoard[cords.row][cords.col] = action.value;
+            const cords = getCordsForIndex(action.field, BOARD_SIZE);
+            newBoard[cords.row][cords.col] = state.player;
             
             return Object.assign({}, state, {
-                board: newBoard
+                board: newBoard,
+                player: state.player === 1 ? -1 : 1
             })
         default:
             return state
